8fb775b83dbc57f3b94d3fb875da034df5a1e75c
Jonas <jonas@jonas-zorin.fritz.box>
2020-08-15 14:49:47 +0200

    added id support

--- index.js
+++ index.js
@@ -5,56 +5,95 @@ function timestamp() {
    return moment().format("YYYY-MM-DD HH:mm:ss");
 }
 
-module.exports = class {
-   constructor(id = null) {
-      this._id = id ? chalk.black.bgGray(` ${id} `) : "";
-   }
-
-   log(content) {
+module.exports = {
+   log: (content) => {
       console.log(
-         `${timestamp()} | ${this._id}${chalk.black.bgGray(
-            " Log   "
-         )} > ${content}`
+         `${timestamp()} | ${chalk.black.bgGray(" Log   ")} > ${content}`
       );
-   }
+   },
 
-   warn(content) {
+   warn: (content) => {
       console.log(
-         `${timestamp()} | ${this._id}${chalk.black.bgYellow(
-            " Warn  "
-         )} > ${content}`
+         `${timestamp()} | ${chalk.black.bgYellow(" Warn  ")} > ${content}`
       );
-   }
+   },
 
-   error(content) {
+   error: (content) => {
       console.log(
-         `${timestamp()} | ${this._id}${chalk.black.bgRed(
-            " Error "
-         )} > ${content}`
+         `${timestamp()} | ${chalk.black.bgRed(" Error ")} > ${content}`
       );
-   }
+   },
 
-   info(content) {
+   info: (content) => {
       console.log(
-         `${timestamp()} | ${this._id}${chalk.black.bgGreenBright(
-            " Info  "
-         )} > ${content}`
+         `${timestamp()} | ${chalk.black.bgGreenBright(" Info  ")} > ${content}`
       );
-   }
+   },
 
-   init(content) {
+   init: (content) => {
       console.log(
-         `${timestamp()} | ${this._id}${chalk.black.bgMagentaBright(
+         `${timestamp()} | ${chalk.black.bgMagentaBright(
             " Init  "
          )} > ${content}`
       );
-   }
+   },
 
-   debug(content) {
+   debug: (content) => {
       console.log(
-         `${timestamp()} | ${this._id}${chalk.black.bgCyanBright(
-            " Debug "
-         )} > ${content}`
+         `${timestamp()} | ${chalk.black.bgCyanBright(" Debug ")} > ${content}`
       );
-   }
+   },
+   class: class {
+      constructor(id = null) {
+         this._id = id ? chalk.black.bgGray(` ${id} `) : "";
+      }
+
+      log(content) {
+         console.log(
+            `${timestamp()} | ${this._id}${chalk.black.bgGray(
+               " Log   "
+            )} > ${content}`
+         );
+      }
+
+      warn(content) {
+         console.log(
+            `${timestamp()} | ${this._id}${chalk.black.bgYellow(
+               " Warn  "
+            )} > ${content}`
+         );
+      }
+
+      error(content) {
+         console.log(
+            `${timestamp()} | ${this._id}${chalk.black.bgRed(
+               " Error "
+            )} > ${content}`
+         );
+      }
+
+      info(content) {
+         console.log(
+            `${timestamp()} | ${this._id}${chalk.black.bgGreenBright(
+               " Info  "
+            )} > ${content}`
+         );
+      }
+
+      init(content) {
+         console.log(
+            `${timestamp()} | ${this._id}${chalk.black.bgMagentaBright(
+               " Init  "
+            )} > ${content}`
+         );
+      }
+
+      debug(content) {
+         console.log(
+            `${timestamp()} | ${this._id}${chalk.black.bgCyanBright(
+               " Debug "
+            )} > ${content}`
+         );
+      }
+   },
 };
